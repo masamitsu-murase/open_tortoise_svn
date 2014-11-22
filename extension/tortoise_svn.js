9d3d7a0f7e0d5f4d07d312ba38a09e123044c217
Masamitsu MURASE <masamitsu.murase@gmail.com>
2014-11-23 03:31:32 +0900

    Update error messages.

--- extension/tortoise_svn.js
+++ extension/tortoise_svn.js
@@ -11,9 +11,18 @@ var gTortoiseSvn = (function(){
 
         chrome.runtime.sendNativeMessage(NATIVE_MESSAGING_HOST, obj, function(response){
             if (chrome.runtime.lastError){
-                console.log("ERROR: " + chrome.runtime.lastError.message + ":" + response);
-            } else {
-                console.log("Messaging host: ", JSON.stringify(response));
+                // console.log("ERROR: " + chrome.runtime.lastError.message + ":" + response);
+                alert(chrome.i18n.getMessage("cannot_open_tortoisesvn_host"));
+                return;
+            }
+
+            // console.log("Messaging host: ", JSON.stringify(response));
+            if (response && response.result){
+                return;
+            }
+
+            if (response){
+                alert(chrome.i18n.getMessage("cannot_open_tortoisesvn") + "\n" + response.error);
             }
         });
 
