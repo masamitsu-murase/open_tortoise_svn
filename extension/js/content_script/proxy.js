4e6c29559f8ca016a81d946a80d888fe844636ac
Masamitsu MURASE <masamitsu.murase@gmail.com>
2016-08-18 01:32:27 +0900

    Use "Proxy" object to send a message from content_script to background.

--- extension/js/content_script/proxy.js
+++ extension/js/content_script/proxy.js
@@ -4,32 +4,26 @@ if (!OpenTsvn) OpenTsvn = {};
 (function(ctx) {
     "use strict";
 
-    const sender_sym = Symbol();
+    // Current implementtion does not support getting property.
+    var proxyCreator = function(sender_name) {
+        // Constructor.
+        return function() {
+            var sender = new ctx.MessageSender(sender_name);
 
-    var TortoiseSvnOpenerProxy = class {
-        constructor() {
-            this[sender_sym] = new ctx.MessageSender("TortoiseSvnOpener");
-        }
-    };
-    [ "openLog", "openRepobrowser", "openBlame" ].forEach(function(func) {
-        TortoiseSvnOpenerProxy.prototype[func] = function() {
-            // async
-            return this[sender_sym].send(func, Array.from(arguments));
+            // If a constructor returns an object, it is returned by "new" statement.
+            return new Proxy({}, {
+              get: function(target, property, receiver) {
+                  return function() {
+                      // async
+                      return sender.send(property, Array.from(arguments));
+                  };
+              }
+            });
         };
-    });
-
-    var BadgeManagerProxy = class {
-        constructor() {
-            this[sender_sym] = new ctx.MessageSender("BadgeManager");
-        }
     };
-    [ "showWarning" ].forEach(function(func) {
-        BadgeManagerProxy.prototype[func] = function() {
-            // async
-            return this[sender_sym].send(func, Array.from(arguments));
-        };
-    });
 
+    var TortoiseSvnOpenerProxy = proxyCreator("TortoiseSvnOpener");
+    var BadgeManagerProxy = proxyCreator("BadgeManager");
 
     ctx.TortoiseSvnOpenerProxy = TortoiseSvnOpenerProxy;
     ctx.BadgeManagerProxy = BadgeManagerProxy;
