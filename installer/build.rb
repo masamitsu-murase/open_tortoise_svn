a6ffaf4d4dc1e9e7fa2ed53a0b86f1091289f567
Masamitsu MURASE <masamitsu.murase@gmail.com>
2016-08-21 21:48:38 +0900

    Update installer, which is based on Ocra.
    Note:
     How to create stubw.exe:
       1. Replace icon images with Resource Hacker.
       2. Add manifest to avoid UAC.
            > mt.exe ?manifest stubw.manifest -outputresource:stubw.exe;1

--- installer/build.rb
+++ installer/build.rb
@@ -1,66 +1,92 @@
+# coding: utf-8
 
 require("fileutils")
 require("pathname")
+require("seven_zip_ruby")
+require("net/http")
+require("uri")
+require("stringio")
 
-INSTALLER_HEADER = <<'EOS'
-@if (0)==(0) echo off
-wscript //E:JScript //Nologo "%~f0" install
-exit
-@end
-EOS
+RUBY_PATH = Pathname(__dir__) + "ruby"
+URL = "http://dl.bintray.com/oneclick/rubyinstaller/ruby-2.2.5-i386-mingw32.7z"
 
-UNINSTALLER_HEADER = <<'EOS'
-@if (0)==(0) echo off
-wscript //E:JScript //Nologo "%~f0" uninstall
-exit
-@end
-EOS
+def fetch(uri_str, limit = 10)
+  # You should choose better exception.
+  raise ArgumentError, 'HTTP redirect too deep' if limit == 0
 
-
-NM_DIR = Pathname("../native_messaging")
-NM_EXE = NM_DIR + "open_tortoise_svn_host.exe"
-NM_JSON = NM_DIR + "open_tortoise_svn.json"
-INSTALL_BAT = "install.bat"
-UNINSTALL_BAT = "uninstall.bat"
-MAIN_JS = "main.js"
-TARGET_DIR = Pathname("../extension")
-TARGET = TARGET_DIR + "open_tortoise_svn_host.zip"
-
-def create_install_bat(exe_data, json_data)
-  File.open(INSTALL_BAT, "w") do |file|
-    file.puts INSTALLER_HEADER
-
-    file.puts("var EXE_DATA = '" + exe_data.unpack("H*")[0] + "';")
-    file.puts("var JSON_DATA = '" + json_data.unpack("H*")[0] + "';")
-
-    file.puts(File.open(MAIN_JS, "r", &:read))
+  response = Net::HTTP.get_response(URI.parse(uri_str))
+  case response
+  when Net::HTTPSuccess
+    response
+  when Net::HTTPRedirection
+    fetch(response['location'], limit - 1)
+  else
+    raise "Error: Invalid HTTP Response"
   end
 end
 
-def create_uninstall_bat
-  File.open(UNINSTALL_BAT, "w") do |file|
-    file.puts UNINSTALLER_HEADER
-    file.puts "var EXE_DATA = '';"
-    file.puts "var JSON_DATA = '';"
-    file.puts(File.open(MAIN_JS, "r", &:read))
+def download_ruby_installer
+  if RUBY_PATH.exist?
+    puts "Ruby is found. Use it."
+    return
   end
-end
 
-def create_zip
-  TARGET.rmtree if TARGET.exist?
-  system("\"C:/Program Files/7-Zip/7z.exe\" a #{TARGET} #{INSTALL_BAT} #{UNINSTALL_BAT}")
+  puts "Downloading..."
+  data = fetch(URL).body.b
+
+  RUBY_PATH.mkpath
+  puts "Extracting..."
+  SevenZipRuby::Reader.open(StringIO.new(data)) do |szr|
+    szr.extract(:all, RUBY_PATH.to_s)
+  end
 end
 
 def main
-  exe_data = File.open(NM_EXE, "rb", &:read).b
-  json_data = File.open(NM_JSON, "rb", &:read).b
-  create_install_bat(exe_data, json_data)
-  create_uninstall_bat
+  download_ruby_installer
 
-  create_zip
+  FileUtils.cp("../native_messaging/open_tortoise_svn.json", "open_tortoise_svn.json")
+  FileUtils.cp("../native_messaging/open_tortoise_svn_host.exe", "open_tortoise_svn_host.exe")
+
+  ENV["PATH"] = Dir.glob(RUBY_PATH.to_s.gsub("\\"){ "/" } + "/*/bin").first.gsub("/"){ "\\" }
+
+  system("gem update --system --no-rdoc --no-ri")
+  system("gem install ocra --version 1.3.6 --no-rdoc --no-ri")
+
+
+  # Note:
+  #  How to create stubw.exe:
+  #   1. Replace icon images with Resource Hacker.
+  #   2. Add manifest to avoid UAC.
+  #        > mt.exe ?manifest stubw.manifest -outputresource:stubw.exe;1
+
+  # Replace stubw.exe.
+  stubw = Dir.glob(RUBY_PATH.to_s.gsub("\\"){ "/" } + "/*/lib/ruby/gems/*/gems/ocra-*/share/ocra/stubw.exe").first
+  FileUtils.cp("stubw.exe", stubw)
+
+  installer = Pathname("install.exe")
+  installer.rmtree if installer.exist?
+  system("ocra --windows --no-enc --gem-minimal --no-autodll install.rb open_tortoise_svn.json open_tortoise_svn_host.exe")
+
+  uninstaller = Pathname("uninstall.exe")
+  uninstaller.rmtree if uninstaller.exist?
+  system("ocra --windows --no-enc --gem-minimal --no-autodll uninstall.rb open_tortoise_svn.json open_tortoise_svn_host.exe")
+
+  FileUtils.cp("install.exe", "../extension/install.exe", verbose: true)
+  FileUtils.cp("uninstall.exe", "../extension/uninstall.exe", verbose: true)
+end
+
+def clean
+  [ "install.exe", "uninstall.exe", "open_tortoise_svn.json", "open_tortoise_svn_host.exe", "ruby" ].each do |item|
+    file = Pathname(item)
+    file.rmtree if file.exist?
+  end
 end
 
 Dir.chdir(__dir__) do
-  main
+  if ARGV[0] == "clean"
+    clean
+  else
+    main
+  end
 end
 
